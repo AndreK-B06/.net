d541a1015465841b375e118b015fca2f2969ed29
André <137167063+AndreK-B06@users.noreply.github.com>
2024-05-31 10:24:26 +0200

    Student GPA task finished

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,7 @@
 ﻿// initialize variables - graded assignments 
+using System.Runtime.CompilerServices;
+using System.Runtime.ConstrainedExecution;
+
 int currentAssignments = 5;
 
 int sophia1 = 93;
@@ -48,4 +51,58 @@ Console.WriteLine("Student\t\tGrand\n");
 Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA");
 Console.WriteLine("Nicolas:\t" + nicolasScore + "\tB");
 Console.WriteLine("Zahirah:\t" + zahirahScore + "\tB");
-Console.WriteLine("Jeong:\t\t" + jeongScore + "\tA");
+Console.WriteLine("Jeong:\t\t" + jeongScore + "\tA\n");
+
+// New task
+string studentName = "Sophia Johnson";
+string course1Name = "English 101";
+string course2Name = "Algebra 101";
+string course3Name = "Biology 101";
+string course4Name = "Computer Science I";
+string course5Name = "Psychology 101";
+
+int course1Credit = 3;
+int course2Credit = 3;
+int course3Credit = 4;
+int course4Credit = 4;
+int course5Credit = 3;
+
+int gradeA = 4;
+int gradeB = 3;
+
+int curse1Grad = gradeA;
+int curse2Grad = gradeB;
+int curse3Grad = gradeB;
+int curse4Grad = gradeB;
+int curse5Grad = gradeA;
+
+int totalCreditHours = 0;
+totalCreditHours += course1Credit;
+totalCreditHours += course2Credit;
+totalCreditHours += course3Credit;
+totalCreditHours += course4Credit;
+totalCreditHours += course5Credit;
+
+int totalGradePoints = 0;
+totalGradePoints += course1Credit * curse1Grad;
+totalGradePoints += course2Credit * curse2Grad;
+totalGradePoints += course3Credit * curse3Grad;
+totalGradePoints += course4Credit * curse4Grad;
+totalGradePoints += course5Credit * curse5Grad;
+
+decimal gradePointAverage = (decimal) totalGradePoints/totalCreditHours;
+
+int leadingDigit = (int) gradePointAverage;
+int firstDigit = (int) (gradePointAverage * 10) % 10;
+int SecondDigit = (int) (gradePointAverage * 100 ) % 10;
+
+//Display the student grads adn credits.
+Console.WriteLine($"Student: {studentName}\n");
+Console.WriteLine("Course\t\t\tGrade\t\tCredit Hours");
+
+Console.WriteLine($"{course1Name}\t\t{curse1Grad}\t\t{course1Credit}");
+Console.WriteLine($"{course2Name}\t\t{curse2Grad}\t\t{course2Credit}");
+Console.WriteLine($"{course3Name}\t\t{curse3Grad}\t\t{course3Credit}");
+Console.WriteLine($"{course4Name}\t{curse4Grad}\t\t{course4Credit}");
+Console.WriteLine($"{course5Name}\t\t{curse5Grad}\t\t{course5Credit}\n");
+Console.WriteLine($"Final GPA: \t\t{leadingDigit}.{firstDigit}{SecondDigit}" );
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2075582d5539bf53afb9e61a4f83ff78151ef8a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52e172a6b7d6f1d1deea8cd271810eff5e2b7bcf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
